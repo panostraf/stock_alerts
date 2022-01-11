a6e84c51bffe372b1cdd153999234aa5922c65b1
panostraf <p.trafalis@gmail.com>
2022-01-11 23:50:32 +0200

    fix __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
-app.secret_key = 'super secret key'
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+def init_app():
+    app = Flask(__name__)
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    
+    return app
+
+app = init_app()
 
 from app import routes
 
--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import app
 
 
 
-if __name__=='__main__':
-	app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
